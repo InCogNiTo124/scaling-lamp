ed505014392bf7cc043fc4661c53c71290c1d720
InCogNiTo124 <msmetko@msmetko.xyz>
2022-06-08 00:40:29 +0200

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 echo "${1}" > /pw
-ansible-vault decrypt key --vault-password-file /pw --output /key_dec
+ansible-vault decrypt /key --vault-password-file /pw --output /key_dec
 chmod 0400 /key_dec
 ansible-playbook -u msmetko -i /inv --vault-password-file /pw --private-key /key_dec /deploy.yaml
