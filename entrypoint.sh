113e3c6c01c216cca1fdbf7553eb90f2c6d3d4cf
InCogNiTo124 <msmetko@msmetko.xyz>
2022-06-12 22:08:39 +0200

    Add support for makefile checking and maybe repo recognition

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 echo "${1}" > /pw
+echo "REPO: ${2}"
 ansible-vault decrypt /key --vault-password-file /pw --output /key_dec
 chmod 0400 /key_dec
 ansible-playbook -u msmetko -i /inv --vault-password-file /pw --private-key /key_dec /deploy.yaml
