28a087377ff14b92e28b0ee7a024bbb46f43f14a
InCogNiTo124 <msmetko@msmetko.xyz>
2022-06-12 22:35:13 +0200

    possible action improvement

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ echo "${1}" > /pw
 echo "REPO: ${2}"
 ansible-vault decrypt /key --vault-password-file /pw --output /key_dec
 chmod 0400 /key_dec
-ansible-playbook -u msmetko -i /inv --vault-password-file /pw --private-key /key_dec /deploy.yaml
+ansible-playbook -u msmetko -i /inv --vault-password-file /pw --private-key /key_dec --extra-vars repo="${GITHUB_REPOSITORY}" /deploy.yaml
