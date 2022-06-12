e7ab9f662982b7d9dae5ff97ce6a2dd24f2b0ee7
InCogNiTo124 <msmetko@msmetko.xyz>
2022-06-12 22:57:20 +0200

    Remove unnecessary input variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 echo "${1}" > /pw
-echo "REPO: ${2}"
 ansible-vault decrypt /key --vault-password-file /pw --output /key_dec
 chmod 0400 /key_dec
 ansible-playbook -u msmetko -i /inv --vault-password-file /pw --private-key /key_dec --extra-vars repo="${GITHUB_REPOSITORY}" /deploy.yaml
